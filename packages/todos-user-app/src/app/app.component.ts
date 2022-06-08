a4493866cb2d078247eb1670a735e443e26f5357
Austin Fahsl <afahsl4@gmail.com>
2022-06-08 13:02:57 -0400

    Use todos api from app

--- packages/todos-user-app/src/app/app.component.ts
+++ packages/todos-user-app/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Todo, TodosApi } from 'todos-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todos-user-app';
+  private readonly _todosApi: TodosApi = new TodosApi();
+
+  todo: Todo = this._todosApi.getTodo();
+
+  newTodo() {
+    this.todo = this._todosApi.getTodo();
+  }
 }
