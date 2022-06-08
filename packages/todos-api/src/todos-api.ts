94e7cde2dc10afe73062b6bcf7cebc0182935e71
Austin Fahsl <afahsl4@gmail.com>
2022-06-08 13:28:07 -0400

    Change todos api implementation and add publish script

--- packages/todos-api/src/todos-api.ts
+++ packages/todos-api/src/todos-api.ts
@@ -4,7 +4,7 @@ import { Todo } from './todo';
 export class TodosApi {
   getTodo(): Todo {
     return {
-      id: faker.datatype.uuid(),
+      id: faker.datatype.uuid() + '-' + faker.datatype.number(10),
       description: faker.company.catchPhrase(),
       isComplete: faker.datatype.boolean(),
     };
